446463654067d947c190034525b3463eec5e4fdb
Valiyev <ali.valiyev@bp.com>
2024-07-31 13:43:07 +0400

    test

--- Ecommers/Ecommers/settings.py
+++ Ecommers/Ecommers/settings.py
@@ -77,17 +77,10 @@ WSGI_APPLICATION = 'Ecommers.wsgi.application'
 
 DATABASES = {
     'default': {
-<<<<<<< HEAD
+
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-=======
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'findaz',
-        'USER': 'myuser',
-        'PASSWORD': 'Ali123',
-        'HOST': 'localhost',
-        'PORT': '5432',
->>>>>>> 0c0317ed178b0c54ef29474edff59b4899be2a9b
+
     }
 }
 
