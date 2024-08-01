61ff4b2c4d95a8f688c4ef34dd8014b646606e36
Valiyev <ali.valiyev@bp.com>
2024-08-01 09:49:17 +0400

    test

--- Ecommers/Ecommers/settings.py
+++ Ecommers/Ecommers/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2j-!zy-xgfd_6osh3==c8^-ry-qqp_78+*6d37d7)26h^bckdi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
